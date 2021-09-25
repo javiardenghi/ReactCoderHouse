b21ccb7f1fa701c1b5969197b274faaf518fb5e9
javiardenghi <72949538+javiardenghi@users.noreply.github.com>
2021-09-25 00:38:41 -0300

    form

--- proyecto-coder/src/Components/Cart/Cart.js
+++ proyecto-coder/src/Components/Cart/Cart.js
@@ -4,25 +4,62 @@ import  Card  from 'react-bootstrap/Card';
 import  Button  from 'react-bootstrap/Button';
 import{useState,useEffect} from "react"
 import {Link} from "react-router-dom"
-
+import { getFirestore } from '../../service/getFirebase';
+import "firebase/firestore" 
 
 function Cart() {
     const [total, setTotal] = useState(0)
+    //estado para formar el array que le paso a"item" en la orden de compra
+    const [producto ,setProducto]= useState([])
+    //product es el array que viene del cartContext
     const {product,clear,removeItem} = useCartContext()
+    let[info, setInfo]=useState({
+        nombre:"",
+        numero:"",
+        email:""
+    })
     function clearArr() {
         clear()
     }
     function borrar(id) {
-        removeItem(id)
-        
+        removeItem(id)   
+    }
+
+    function datosChange(e) {
+        setInfo({
+            ...info,
+            [e.target.name]:e.target.value
+        })
+        console.log(info)
+    }
+
+    function datosSubmit(e) {
+        e.preventDefault()
+
+        const order ={
+            buyer: info,
+            items: producto,
+            total: total
+        }
+        const db = getFirestore()
+        const orders = db.collection("ordenes")
+
+        orders.add(order).then(res => console.log(res))
+        .catch(console.log("error en orden"))
+        .finally(()=>setInfo({
+            nombre:"",
+            numero:"",
+            email:""
+        }))
     }
 
     useEffect(() => {
         
        setTotal(product.reduce((acc,e)=> acc+e.e.data().precio*e.quantity,0))
-    
+       product.map(e=> setProducto([...producto, e.e.data()]))
           
     }, [product])
+
 
     return (
       
@@ -51,13 +88,21 @@ function Cart() {
             </Card>
    
             </ListGroup.Item>
-
             </>
-            )
-            }
+            ) }
         </ListGroup>
+
         <h1>Total: ${total}</h1>
         <button onClick={clearArr}>Clear</button>
+
+        <form onChange={datosChange} onSubmit={datosSubmit}>
+
+            <input type="text" placeholder="Nombre Completo" name="nombre" value = {info.nombre} />
+            <input type="text" placeholder="Telefono" name="numero" value = {info.numero} />
+            <input type="text" placeholder="Email" name="email" value = {info.email} />
+            <button>Terminar compra</button>
+
+        </form>
 
     </div>
     )
